377b335a5cd219c78452ed64081650403b28d442
Yu Kuan-Hsun <glenyu80119@gmail.com>
2019-08-09 20:33:47 +0800

    add line bot: third trial, remove helloworld

--- hello.py
+++ hello.py
@@ -14,10 +14,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('AooJdMsRyq6LV+OgOLDwVI+GSUsbG4CwGi2mV4nBq2TQgHC6OO8odH6H1Jm0BDK01dkTH4C3JZ8CH9EdqAHZpgrw4yAKP6D4b7gofQrmVrRu5c0dAYhGT3rNgbsklKbZ0S7eUwJHE/uqnZOaGXC56wdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('30d9b4930e5eaa9cac656c533977e295')
 
-@app.route('/')
-def hello():
-	return 'Hello, World!'
-
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
