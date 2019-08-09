2146e40d060058a7d1632624d060c9436632e613
Yu Kuan-Hsun <glenyu80119@gmail.com>
2019-08-09 21:00:42 +0800

    add line bot: fourth trial: remove MessageEvent handler for verification trial2

--- hello.py
+++ hello.py
@@ -23,12 +23,6 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
 
     return 'OK'
 
