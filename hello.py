af76020bab875db0c1d641f925f64b231342c39a
Yu Kuan-Hsun <glenyu80119@gmail.com>
2019-08-09 20:56:54 +0800

    add line bot: fourth trial: remove MessageEvent handler for verification

--- hello.py
+++ hello.py
@@ -11,8 +11,8 @@ from linebot.models import (
 )
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('AooJdMsRyq6LV+OgOLDwVI+GSUsbG4CwGi2mV4nBq2TQgHC6OO8odH6H1Jm0BDK01dkTH4C3JZ8CH9EdqAHZpgrw4yAKP6D4b7gofQrmVrRu5c0dAYhGT3rNgbsklKbZ0S7eUwJHE/uqnZOaGXC56wdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('30d9b4930e5eaa9cac656c533977e295')
+line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('CHANNEL_SECRET')
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -32,12 +32,6 @@ def callback():
 
     return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
