6e26fdcba62c7db7fdc2ffd25a463533506f6820
Yu Kuan-Hsun <glenyu80119@gmail.com>
2019-08-09 21:26:34 +0800

    add line bot: success, add a great song, call it a day

--- hello.py
+++ hello.py
@@ -41,7 +41,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text="I know nothing but a good song now, https://reurl.cc/4Edb2"))
 
 
 if __name__ == "__main__":
